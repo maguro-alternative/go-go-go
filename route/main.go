bfdf4ca85c7654ad17db65c283ed5a54536171a0
ygo <ka.ra.si.naporitan@gmail.com>
2024-08-02 07:59:22 +0900

    とりあえずやり取り

--- route/main.go
+++ route/main.go
@@ -1,1 +1,12 @@
 package route
+
+import (
+	"net/http"
+
+	"maguro-alternative/go-go-go/route/todo"
+)
+
+func Routes() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", todo.Todo)
+}
--- route/todo/todo.go
+++ route/todo/todo.go
@@ -1,1 +1,9 @@
 package todo
+
+import (
+	"net/http"
+)
+
+func Todo(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}

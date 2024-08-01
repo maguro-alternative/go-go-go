cbeb1b13f93df4bee14a0585da6fadd6aa2b9a90
ygo <ka.ra.si.naporitan@gmail.com>
2024-08-02 08:45:40 +0900

    とりあえずroute作成

--- route/main.go
+++ route/main.go
@@ -4,9 +4,14 @@ import (
 	"net/http"
 
 	"maguro-alternative/go-go-go/route/todo"
+	"maguro-alternative/go-go-go/repository"
 )
 
-func Routes() {
+func Routes(repo repository.Repository) *http.ServeMux {
 	mux := http.NewServeMux()
+	todo := todo.NewTodoService(
+		repo,
+	)
 	mux.HandleFunc("/", todo.Todo)
+	return mux
 }

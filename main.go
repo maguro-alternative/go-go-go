a5ae91c8a69208e23896c89d317a3a9d0a9eabe8
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2024-08-05 23:06:37 +0900

    ping追加

--- main.go
+++ main.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"maguro-alternative/go-go-go/route"
+)
+
+func main() {
+	r := route.Routes()
+	r.Run(":8080")
+}
--- route/main.go
+++ route/main.go
@@ -1,17 +1,13 @@
 package route
 
-import (
-	"net/http"
+import "github.com/gin-gonic/gin"
 
-	"maguro-alternative/go-go-go/route/todo"
-	"maguro-alternative/go-go-go/repository"
-)
-
-func Routes(repo repository.Repository) *http.ServeMux {
-	mux := http.NewServeMux()
-	todo := todo.NewTodoService(
-		repo,
-	)
-	mux.HandleFunc("/", todo.Todo)
-	return mux
+func Routes() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	return r
 }

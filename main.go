3e9821efb46c4e3542e79acaacf708144b71695f
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2024-08-06 16:20:29 +0900

    db追加

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"maguro-alternative/go-go-go/model"
+	"maguro-alternative/go-go-go/pkg/db"
 	"maguro-alternative/go-go-go/route"
 )
 
 func main() {
+	db, err := db.ConnectionDB()
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to connect to database: %v", err))
+	}
+
+	// Migrate the schema
+	err = db.AutoMigrate(&model.Todo{})
+	if err != nil {
+		panic(fmt.Sprintf("Failed to migrate database: %v", err))
+	}
 	r := route.Routes()
 	r.Run(":8080")
 }

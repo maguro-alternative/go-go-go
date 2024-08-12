586bbb786744bf18b2f51c60cec3d490d6f92a2e
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2024-08-12 11:17:19 +0900

    APIの追加

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"net/http"
+	"encoding/json"
 )
 
 type Todo struct {
@@ -35,8 +37,6 @@ func getDBConfig() DBConfig {
     }
 }
 
-
-
 func connectionDB() (*gorm.DB, error) {
 	config := getDBConfig();
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True", config.User, config.Password, config.Host, config.Port, config.Table)
@@ -44,6 +44,59 @@ func connectionDB() (*gorm.DB, error) {
 	return db, err
 }
 
+func errorDB(db *gorm.DB, c *gin.Context) bool {
+	if db.Error != nil {
+		log.Printf("Error todos: %v", db.Error)
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return true // エラーがあったことを示す
+	}
+	return false // エラーがなかったことを示す
+}
+
+func listeners(r *gin.Engine, db *gorm.DB) {
+	r.GET("/todo/delete", func(c *gin.Context) {
+		id, _ := c.GetQuery("id")
+		result := db.Delete(&Todo{}, id)
+		if errorDB(result, c) { return }
+	})
+
+	r.POST("/todo/update", func(c *gin.Context) {
+		id, _ := strconv.Atoi(c.PostForm("id"))
+		content := c.PostForm("content")
+		var todo Todo
+		result := db.Where("id = ?", id).Take(&todo)
+		if errorDB(result, c) { return }
+		todo.Content = content
+		result = db.Save(&todo)
+		if errorDB(result, c) { return }
+	})
+
+	r.POST("/todo/create", func(c *gin.Context) {
+		content := c.PostForm("content")
+		fmt.Println(c.Request.PostForm, content)
+		result := db.Create(&Todo{Content: content})
+		if errorDB(result, c) { return }
+	})
+
+	r.GET("/todo/list", func(c *gin.Context) {
+		var todos []Todo
+		// Get all records
+		result := db.Find(&todos)
+		if errorDB(result, c) { return }
+		fmt.Println(json.NewEncoder(os.Stdout).Encode(todos))
+		c.JSON(http.StatusOK, todos)
+	})
+
+	r.GET("/todo/get", func(c *gin.Context) {
+		var todo Todo
+		id, _ := c.GetQuery("id")
+		result := db.First(&todo, id)
+		if errorDB(result, c) { return }
+		// JSON形式でレスポンスを返す
+		fmt.Println(json.NewEncoder(os.Stdout).Encode(todo))
+		c.JSON(http.StatusOK, todo)
+	})
+}
 
 func main() {
 	r := gin.Default()
@@ -59,11 +112,7 @@ func main() {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	listeners(r, db)
 
 	fmt.Println("Database connection and setup successful")
 	r.Run(":8080")
